fd52980a0a77516fd751db1795f64dfb7d3affdf
adleywd <adleydamaceno@gmail.com>
2022-09-24 18:45:02 +0100

    Add header and footer to replacement

--- WordReplacer/Utilities/DocumentHelper.cs
+++ WordReplacer/Utilities/DocumentHelper.cs
@@ -11,6 +11,99 @@ namespace WordReplacer.Utilities;
 public static class DocumentHelper
 {
     /// <summary>
+    /// It replaces the values in the dictionary.
+    /// </summary>
+    /// <param name="values">A dictionary of key/value pairs. The key is the name of the variable to replace, and the value
+    /// is the value to replace it with.</param>
+    /// <param name="streamFile">The file to be replaced.</param>
+    public static Stream? Replace(Dictionary<string, string> values, MemoryStream? streamFile)
+    {
+        try
+        {
+            if (streamFile is not null)
+            {
+                using (var wordDoc = WordprocessingDocument.Open(streamFile, true))
+                {
+                    ReplaceWordBodyText(wordDoc, values);
+                    ReplaceWordHeaderText(wordDoc, values);
+                    ReplaceWordFooterText(wordDoc, values);
+                    wordDoc.Close();
+                }
+
+                return streamFile;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Replaces the body text from Word file.
+    /// </summary>
+    /// <param name="doc">The document.</param>
+    /// <param name="replaceWords">The replace words.</param>
+    private static void ReplaceWordBodyText(WordprocessingDocument doc, Dictionary<string, string> replaceWords)
+    {
+        Body? body = doc.MainDocumentPart?.Document.Body;
+
+        if (body is null)
+        {
+            return;
+        }
+
+        foreach (var text in body.Descendants<Text>())
+        {
+            foreach (var words in replaceWords)
+            {
+                if (text.Text.Contains(words.Key))
+                {
+                    text.Text = text.Text.ReplaceTextWithRegex(words.Key, words.Value, true, false);
+                }
+            }
+        }
+    }
+
+    private static void ReplaceWordHeaderText(WordprocessingDocument doc, Dictionary<string, string> replaceWords)
+    {
+        foreach (var headerPart in doc.MainDocumentPart.HeaderParts)
+        {
+            foreach (var text in headerPart.RootElement.Descendants<Text>())
+            {
+                foreach (var words in replaceWords)
+                {
+                    if (text.Text.Contains(words.Key))
+                    {
+                        text.Text = text.Text.ReplaceTextWithRegex(words.Key, words.Value, true, false);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void ReplaceWordFooterText(WordprocessingDocument doc, Dictionary<string, string> replaceWords)
+    {
+        foreach (var headerPart in doc.MainDocumentPart.FooterParts)
+        {
+            foreach (var text in headerPart.RootElement.Descendants<Text>())
+            {
+                foreach (var words in replaceWords)
+                {
+                    if (text.Text.Contains(words.Key))
+                    {
+                        text.Text = text.Text.ReplaceTextWithRegex(words.Key, words.Value, true, false);
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
     /// Given a list of nodes, a current node index, a result list, and a current dictionary, add to the result list all
     /// possible combinations of nodes that can be created by traversing the nodes in the list starting at the current node
     /// index, and using the current dictionary to store the values of the nodes that have been traversed
@@ -63,58 +156,6 @@ public static class DocumentHelper
             {
                 currentDict.Add(currentNode.Key, value);
                 GetCombinations(nodes, currentNodeIdx + 1, resultList, currentDict);
-            }
-        }
-    }
-
-    /// <summary>
-    /// It replaces the values in the dictionary.
-    /// </summary>
-    /// <param name="values">A dictionary of key/value pairs. The key is the name of the variable to replace, and the value
-    /// is the value to replace it with.</param>
-    /// <param name="streamFile">The file to be replaced.</param>
-    public static Stream? Replace(Dictionary<string, string> values, MemoryStream? streamFile)
-    {
-        try
-        {
-            if (streamFile is not null)
-            {
-                using (var wordDoc = WordprocessingDocument.Open(streamFile, true))
-                {
-                    ReplaceWordBodyText(wordDoc, values);
-                    wordDoc.Close();
-                }
-
-                return streamFile;
-            }
-            else
-            {
-                return null;
-            }
-        }
-        catch (Exception ex)
-        {
-            return null;
-        }
-    }
-
-    private static void ReplaceWordBodyText(WordprocessingDocument doc, Dictionary<string, string> replaceWords)
-    {
-        Body? body = doc.MainDocumentPart?.Document.Body;
-
-        if (body is null)
-        {
-            return;
-        }
-
-        foreach (var text in body.Descendants<Text>())
-        {
-            foreach (var words in replaceWords)
-            {
-                if (text.Text.Contains(words.Key))
-                {
-                    text.Text = text.Text.ReplaceTextWithRegex(words.Key, words.Value, true, false);
-                }
             }
         }
     }
