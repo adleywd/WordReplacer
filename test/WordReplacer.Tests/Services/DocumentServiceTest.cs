376798ab8d7bf8b964fd51faba1b80302a2660cb
adleywd <adleydamaceno@gmail.com>
2022-10-20 00:44:04 +0100

    Update GetCombinations Test

--- test/WordReplacer.Tests/Services/DocumentServiceTest.cs
+++ test/WordReplacer.Tests/Services/DocumentServiceTest.cs
@@ -1,6 +1,5 @@
 using System.Text.Json;
 using WordReplacer.Common;
-using WordReplacer.Models;
 
 namespace WordReplacer.Tests.Services;
 
@@ -10,7 +9,7 @@ public class DocumentServiceTest
     private void GetCombinations_WithValidNodeList_ShouldReturnListWithAllPossibleCombinationsInDict()
     {
         // Arrange
-        var expectedListResult = new List<IDictionary<string, string>>
+        var expectedListResult = new List<Dictionary<string, string>>
         {
             new Dictionary<string, string>
             {
@@ -32,9 +31,9 @@ public class DocumentServiceTest
 
         List<KeyValuePair<string, List<string>>> nodes = new()
         {
-            new ("Student", new List<string>() { "A1", "A2" } ),
-            new ("School", new List<string>() { "E1" } ),
-            new ("Date", new List<string>() { "D1", "D2" } )
+            new KeyValuePair<string, List<string>>("Student", new List<string>() { "A1", "A2" } ),
+            new KeyValuePair<string, List<string>>("School", new List<string>() { "E1" } ),
+            new KeyValuePair<string, List<string>>("Date", new List<string>() { "D1", "D2" } )
         };
 
         var result = new List<Dictionary<string, string>>();
@@ -43,9 +42,23 @@ public class DocumentServiceTest
         result.GetCombinations(nodes, 0, new Dictionary<string, string>());
 
         // Assert
-        var serializedResult = JsonSerializer.Serialize(result);
-        var serializedExpectedResult = JsonSerializer.Serialize(expectedListResult);
-        serializedResult.ShouldBe(serializedExpectedResult);
+        (SerializeDictOrdered(result) == SerializeDictOrdered(expectedListResult)).ShouldBeTrue();
     }
-    
+
+    private static string SerializeDictOrdered(IList<Dictionary<string, string>> list)
+    {
+        var result = new List<KeyValuePair<string, string>>();
+        
+        foreach (var item in list)
+        {
+            foreach (var keyValue in item)
+            {
+                result.Add(new KeyValuePair<string, string>(keyValue.Key, keyValue.Value));
+            }
+        }
+
+        var orderedResult = result.OrderBy(kv => kv.Key).ThenBy(kv => kv.Value);
+        return JsonSerializer.Serialize(orderedResult);
+    }
+
 }
